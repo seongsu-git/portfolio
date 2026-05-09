87666673ce99027a28b016a937f781bc89df5967
seongsu-git <seongsu0221@gmail.com>
2026-05-10 04:30:59 +0900

    CopyButton props 수정

--- src/app/component/client/Button.tsx
+++ src/app/component/client/Button.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-export function CopyButton({ email }: { email: string }) {
+export function CopyButton({ text }: { text: string }) {
     return (
         <button
             type="button"
             className="button-copy"
             onClick={() => {
-                navigator.clipboard.writeText(email);
+                navigator.clipboard.writeText(text);
                 //toast 라이브러리 사용검토
                 alert("읽어주셔서 감사합니다. 복사되었습니다.");
             }}
         >
-            {email}
+            {text}
         </button>
     );
 }
